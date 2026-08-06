1272300643e0b34f8d824702460323101502e40c
br1ansouza <briandesouza1597@gmail.com>
2026-08-06 17:45:44 -0300

    feat: adiciona certificados novos ordenados por ano

--- src/lib/data/certificates.ts
+++ src/lib/data/certificates.ts
@@ -14,10 +14,22 @@ export const certificates: Certificate[] = [
     link: 'https://drive.google.com/file/d/1fQTwMoMofadk9GqVy6hvvqSwRRiRbimX/view?usp=drive_link',
   },
   {
-    name: 'Full Stack - Módulo 3 - LAB365',
+    name: 'Full Stack · Módulo 3',
     institution: 'LAB365',
     date: '2025',
     link: 'https://drive.google.com/file/d/1BjzgSB1mwL47Iydj0eKe78qbuIFod89j/view?usp=sharing',
+  },
+  {
+    name: 'Back-end · Módulo 2',
+    institution: 'LAB365',
+    date: '2025',
+    link: 'https://drive.google.com/file/d/1iL9Pg_g_AwZO15JNWQqso0OJFRXS73Ls/view?usp=sharing',
+  },
+  {
+    name: 'Front-end · Módulo 1',
+    institution: 'LAB365',
+    date: '2025',
+    link: 'https://drive.google.com/file/d/1-XHxEg9cCRRAyv5e_1Io2noM_zjEIgb1/view?usp=sharing',
   },
   {
     name: 'Ambientes Computacionais e Conectividade',
@@ -26,22 +38,22 @@ export const certificates: Certificate[] = [
     link: 'https://drive.google.com/file/d/151hXSNY0QEshYFQBHKtpyaFK0k0wdHk9/view?usp=sharing',
   },
   {
-    name: 'BackEnd - Módulo 2 - LAB365',
-    institution: 'LAB365',
-    date: '2025',
-    link: 'https://drive.google.com/file/d/1iL9Pg_g_AwZO15JNWQqso0OJFRXS73Ls/view?usp=sharing',
-  },
-  {
-    name: 'FrontEnd - Módulo 1 - LAB365',
-    institution: 'LAB365',
-    date: '2024',
-    link: 'https://drive.google.com/file/d/1-XHxEg9cCRRAyv5e_1Io2noM_zjEIgb1/view?usp=sharing',
-  },
-  {
     name: 'Desenvolvimento Web Compacto e Completo',
     institution: 'Udemy',
     date: '2023',
     link: 'https://drive.google.com/file/d/1F7iHWIBZL1z_ESQ8g4nn_zHeedvTjOjF/view?usp=sharing',
+  },
+  {
+    name: 'Arquitetura de Redes',
+    institution: 'Udemy',
+    date: '2023',
+    link: 'https://drive.google.com/file/d/1u-_zNT4k-cAbBc5vBg73ONCtUj-3-jxD/view?usp=sharing',
+  },
+  {
+    name: 'Proxmox · Professional',
+    institution: 'Udemy',
+    date: '2023',
+    link: 'https://drive.google.com/file/d/1xQC7fbBYRnFmgEG8sqMlX8-rObgjOuQ5/view?usp=sharing',
   },
   {
     name: 'Fundamentos de Programação',
@@ -54,17 +66,5 @@ export const certificates: Certificate[] = [
     institution: 'Udemy',
     date: '2022',
     link: 'https://drive.google.com/file/d/1Kgrh015VJbjAs9087GGBf9vQxZEhaNFT/view?usp=sharing',
-  },
-  {
-    name: 'Arquitetura de Redes',
-    institution: 'Udemy',
-    date: '2023',
-    link: 'https://drive.google.com/file/d/1u-_zNT4k-cAbBc5vBg73ONCtUj-3-jxD/view?usp=sharing',
-  },
-  {
-    name: 'Proxmox - Professional',
-    institution: 'Udemy',
-    date: '2023',
-    link: 'https://drive.google.com/file/d/1xQC7fbBYRnFmgEG8sqMlX8-rObgjOuQ5/view?usp=sharing',
   },
 ];
